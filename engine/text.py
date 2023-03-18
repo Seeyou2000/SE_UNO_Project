84be25fe48b21483137e1f7e17300dd34a03b724
Gidong Kwon <lucidfext@gmail.com>
2023-03-19 04:12:40 +0900

    position 대신 rect로 위치 관리 통일

--- engine/text.py
+++ engine/text.py
@@ -4,19 +4,18 @@ from engine.gameobject import GameObject
 class Text(GameObject):
     font: pygame.font.Font
     _rendered_text: pygame.Surface
-    position: pygame.Vector2
     color: pygame.Color
     
     def __init__(self, text: str, position: pygame.Vector2, font: pygame.font.Font, color: pygame.Color):
         super().__init__()
         self.text = text
-        self.position = position
         self.font = font
         self.color = color
+        self.rect = pygame.Rect(position, (0, 0))
         self.set_text(text)
 
     def render(self, surface: pygame.Surface):
-        surface.blit(self._rendered_text, self.position)
+        surface.blit(self._rendered_text, self.rect.topleft)
 
     def set_text(self, text: str):
         self._rendered_text = self.font.render(text, True, self.color)
