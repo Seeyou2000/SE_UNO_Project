155243d6241519d6558a1f545c4dac62417fff6e
Kimsiwoo <valentine0517@naver.com>
2023-03-31 17:13:36 +0900

    기능카드 노드 공격부분 정상구동 확인, 숫자카드 오류 확인 후 해결

---
+++ game/gameplay/flow/abilitycard_test.py
@@ -0,0 +1,35 @@
+import pygame
+
+from game.gameplay.flow.abilitycard import AbilityCardFlowNode
+from game.gameplay.flow.gameflowmachine import GameFlowMachine
+from game.gameplay.flow.gamestart import GameStartFlowNode
+from game.gameplay.gamestate import GameState
+from game.scene.card import Card
+from game.scene.constant import NAME, ColorableAbilityType
+from game.scene.player import Player
+
+
+def test_abilitycard():
+    pygame.font.init()
+    player_count = 2
+    game_state = GameState()
+    flow = GameFlowMachine()
+    flow.transition_to(
+        GameStartFlowNode(game_state, [Player(name) for name in NAME[:player_count]])
+    )
+    game_state.now_color = "red"
+    game_state.players[0].cards[0] = Card(
+        "red", None, ColorableAbilityType.GIVE_FOUR_CARDS
+    )
+
+    game_state.players[1].cards = [Card("red", 1, None) for _ in range(0, 7)]
+    # before_cardlen = len(game_state.players[0].cards)
+    # before_turn = game_state.turn.current
+
+    before_attacked_player_cards = len(game_state.players[1].cards)
+
+    flow.transition_to(AbilityCardFlowNode(game_state, game_state.players[0].cards[0]))
+    # after_cardlen = len(game_state.players[0].cards)
+    after_attacked_player_cards = len(game_state.players[1].cards)
+
+    assert (before_attacked_player_cards + 4) == after_attacked_player_cards
--- game/gameplay/flow/numbercard_test.py
+++ game/gameplay/flow/numbercard_test.py
@@ -17,12 +17,12 @@ def test_numbercard():
         GameStartFlowNode(game_state, [Player(name) for name in NAME[:player_count]])
     )
     # game_state.drawn_deck.get_last().number = 1
-    game_state.drawn_deck.get_last().color = "red"
+    game_state.now_color = "red"
     # game_state.players[0].cards[0].number = 1
     game_state.players[0].cards[0].color = "red"
     before_cardlen = len(game_state.players[0].cards)
     before_turn = game_state.turn.current
     flow.transition_to(NumberCardFlowNode(game_state, game_state.players[0].cards[0]))
     after_cardlen = len(game_state.players[0].cards)
-    assert before_cardlen == after_cardlen + 1
+    assert before_cardlen == after_cardlen + 1  # "카드가 잘 내졌는지(이전보다 한 장 줄었는지) 검사"
     assert before_turn + 1 == game_state.turn.current
