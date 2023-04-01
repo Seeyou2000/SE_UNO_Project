2fb36571cba4d6bb98a8f0a9e5f3919add585c55
whuchubird <85631857+whuchubird@users.noreply.github.com>
2023-04-01 18:03:41 +0900

    각 플레이어들의 배치에 따른 카드 표시

--- game/scene/ingame.py
+++ game/scene/ingame.py
@@ -45,14 +45,14 @@ class InGameScene(Scene):
         self.hand = pygame.Rect(0, 500, 600, 100)
         self.otherplayers = pygame.Rect(600, 0, 200, 500)
         self.otherplayer = []
-        for i in range(0, self.player_count):
-            self.otherplayer.append(pygame.Rect(600, i * 83, 200, 83))
+        for i in range(0, self.player_count - 1):
+            self.otherplayer.append(pygame.Rect(600, i * 100, 200, 100))
         self.otherplayertext = []
-        for i in range(0, self.player_count):
+        for i in range(0, self.player_count - 1):
             self.otherplayertext.append(
                 Button(
-                    self.name[i],
-                    pygame.Rect(600, i * 83, 200, 20),
+                    self.name[i + 1],
+                    pygame.Rect(600, i * 100, 200, 20),
                     pygame.font.SysFont("Arial", 20),
                 )
             )
@@ -76,11 +76,11 @@ class InGameScene(Scene):
     def render(self, surface: pygame.Surface) -> None:
         pygame.draw.rect(surface, (80, 188, 223, 0), self.hand)
         pygame.draw.rect(surface, (100, 100, 100, 0), self.otherplayers)
-        for i in range(0, self.player_count):
+        for i in range(0, self.player_count - 1):
             pygame.draw.rect(surface, (230, 230, 230, 0), self.otherplayer[i])
         super().render(surface)
 
-    def handle_flow(self, event: TransitionEvent) -> None:
+    """def handle_flow(self, event: TransitionEvent) -> None:
         match event.transition_from, event.transition_to:
             case _, "TurnStartFlowNode":
                 self.add_children(self.game_state.get_current_player().cards)
@@ -91,7 +91,25 @@ class InGameScene(Scene):
                 for i, card in enumerate(self.game_state.get_current_player().cards):
                     card.rect.center = (50 + 50 * i, 550)
                     card.off("click")
-                    card.on("click", self.handle_click_card(card))
+                    card.on("click", self.handle_click_card(card))"""
+
+    def handle_flow(self, event: TransitionEvent) -> None:
+        match event.transition_from, event.transition_to:
+            case _, "TurnStartFlowNode":
+                self.add_children(self.game_state.get_current_player().cards)
+                self.add_child(self.game_state.drawn_deck.get_last())
+                self.game_state.drawn_deck.get_last().rect = self.deck_button.rect.move(
+                    60, 0
+                )
+                for j in range(0, self.player_count):
+                    for i, card in enumerate(self.game_state.players[j].cards):
+                        if j == 0:
+                            card.rect.center = (50 + 50 * i, 550)
+                            card.off("click")
+                            card.on("click", self.handle_click_card(card))
+                        else:
+                            card.rect = pygame.Rect(0, 0, 12, 20)
+                            card.rect.center = (610 + 13 * i, (j - 1) * 100 + 60)
 
     def handle_click_card(self, card: Card) -> None:
         def handler(event: Event) -> None:
