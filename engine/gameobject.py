f243a65288bc94ec2a7088b6bc713b6e7ab52180
Gidong Kwon <lucidfext@gmail.com>
2023-04-04 16:57:26 +0900

    mouse_enter를 추가하면서 mouse_out이 빠져나갈 때 단 한 번만 emit되도록 변경

--- engine/gameobject.py
+++ engine/gameobject.py
@@ -19,7 +19,7 @@ class GameObject(EventEmitter, abc.ABC):
         self.on("global_mouse_down", self.handle_global_mouse_down)
         self.on("global_mouse_up", self.handle_global_mouse_up)
         self.on("global_mouse_move", self.handle_global_mouse_move)
-        self.on("mouse_out", self.handle_mouse_out)
+        self.on("front_object_entered", self.handle_front_object_entered)
 
     def update(self, dt: float) -> None:
         pass
@@ -40,17 +40,41 @@ class GameObject(EventEmitter, abc.ABC):
         self._is_pressed = False
 
     def handle_global_mouse_move(self, event: Event) -> None:
+        # 매 프레임 실행된다.
         if event.target is self:
             return
+        is_first_enter = False
+        is_first_exit = False
         if "pos" in event.data:
-            self._is_hovered = self.rect.collidepoint(event.data.get("pos"))
-        if self._is_hovered:
+            is_mouse_collided = self.rect.collidepoint(event.data.get("pos"))
+            is_first_enter = (not self._is_hovered) and is_mouse_collided
+            is_first_exit = self._is_hovered and (not is_mouse_collided)
+            self._is_hovered = is_mouse_collided
+        if is_first_enter:
+            self.emit("mouse_enter", Event(event.data))
+
             out_event = Event(None)
             out_event.target = self
-            self.parent.emit("mouse_out", out_event, False)
 
+            root = self.parent
+            while root.parent is not None:
+                root = root.parent
+            root.emit("front_object_entered", out_event, False)
+
+        if is_first_exit:
+            self.emit("mouse_out", Event(None))
+
+        if self._is_hovered:
             self.emit("hover", event)
             event.stop_propagation()
 
-    def handle_mouse_out(self, event: Event) -> None:
+    def handle_front_object_entered(self, event: Event) -> None:
+        if event.target is self:
+            return
+        if not self._is_hovered:
+            return
+
+        event.stop_propagation()
+
         self._is_hovered = False
+        self.emit("mouse_out", Event(None))
