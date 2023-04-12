4d5157ad4ad1f37dc11bc41d1754890eb63bc4f6
Gidong Kwon <lucidfext@gmail.com>
2023-04-13 05:10:19 +0900

    기본 프레임 120으로 수정

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from game.menu.menuscene import MenuScene
 
 
 def main() -> None:
-    world = World((800, 600), 60)
+    world = World((800, 600), 120)
     world.settings.load()
     world.director.change_scene(MenuScene(world))
 
