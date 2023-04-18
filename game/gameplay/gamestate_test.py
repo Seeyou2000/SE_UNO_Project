9067c2d42cc87915c6cc9fa99626b4eddba05a7f
Gidong Kwon <lucidfext@gmail.com>
2023-04-18 10:34:21 +0900

    확률 1000회 유닛테스트 추가

--- game/gameplay/gamestate_test.py
+++ game/gameplay/gamestate_test.py
@@ -5,12 +5,26 @@ from game.gameplay.gamestate import GameState
 from game.gameplay.player import Player
 
 
-def test_gamestate() -> None:
-    pygame.font.init()
+def test_weighted_draw() -> None:
     player_count = 2
     game_state = GameState()
     game_state.reset([Player(name) for name in NAME[:player_count]])
-    before_player = game_state.get_current_player()
-    game_state.turn.next()
-    next_player = game_state.get_current_player()
-    assert before_player != next_player
+
+    # 로직상의 매직넘버
+    weighted_cards_at_once = 7
+    ability_count = 0
+    number_count = 0
+    for i in range(0, round(1000 / weighted_cards_at_once)):
+        cards = game_state.weighty_draw_cards()
+        for _ in range(0, weighted_cards_at_once):
+            card = cards.pop()
+            if card.ability is not None:
+                ability_count += 1
+            elif card.number is not None:
+                number_count += 1
+
+    print(ability_count, number_count)
+
+    tolerance = 100
+    target = 600
+    assert abs(ability_count - target) <= tolerance
