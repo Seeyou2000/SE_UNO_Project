cace932bee9b3400c8d5217d7556c883be2ff257
SHINDONGHOON <dongseoultech@gmail.com>
2023-05-09 01:54:34 +0900

    업적이 달성됐을 때 실시간으로 반영되게 하기 및 10턴 안에 승리 시 업적 달성

--- game/gameplay/flow/gameend.py
+++ game/gameplay/flow/gameend.py
@@ -1,3 +1,4 @@
+from engine.events.event import Event
 from game.gameplay.flow.abstractflownode import AbstractGameFlowNode
 from game.gameplay.gamestate import GameState
 
@@ -5,3 +6,15 @@ from game.gameplay.gamestate import GameState
 class GameEndFlowNode(AbstractGameFlowNode):
     def __init__(self, game_state: GameState) -> None:
         super().__init__(game_state)
+
+    def enter(self) -> None:
+        super().enter()
+        self.machine.events.emit(
+            "game_end",
+            Event(
+                {
+                    "turn": self.game_state.turn.current,
+                    "player": self.game_state.get_current_player(),
+                }
+            ),
+        )
