f5807be5bfd57b2f411e7d6020c8fe06c09241fa
SHINDONGHOON <dongseoultech@gmail.com>
2023-05-09 01:56:55 +0900

    주석 수정

--- game/achievements.py
+++ game/achievements.py
@@ -31,7 +31,7 @@ class Achievements(EventEmitter):
             with open(ACHIEVEMENTS_FILE_PATH) as f:
                 self.load_dict(json.load(f))
         except FileNotFoundError:
-            print("기존 설정 파일을 찾지 못했습니다. 새 설정 파일을 만듭니다.")
+            print("기존 업적 파일을 찾지 못했습니다. 새 업적 파일을 만듭니다.")
             self.reset()
 
     def load_dict(self, value: dict[str, Any]) -> None:
@@ -42,9 +42,9 @@ class Achievements(EventEmitter):
         except (AttributeError, KeyError) as e:
             raise e
             if value is DEFAULT_ACHIEVEMENTS:
-                print("기본 설정값을 불러오는 데 문제가 있습니다. 키값을 점검하세요.")
+                print("기본 업적값을 불러오는 데 문제가 있습니다. 키값을 점검하세요.")
             else:
-                print("저장된 설정을 불러오는 데 문제가 있어 기본 설정값으로 대체합니다.")
+                print("저장된 업적을 불러오는 데 문제가 있어 기본 업적값으로 대체합니다.")
                 self.reset()
 
     @property
