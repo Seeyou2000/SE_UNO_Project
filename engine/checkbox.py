0a7167c832552c6ece2baca2cfa16c665eb20fb0
Gidong Kwon <lucidfext@gmail.com>
2023-05-17 22:35:04 +0900

    체크박스 모양새 수정

--- engine/checkbox.py
+++ engine/checkbox.py
@@ -3,26 +3,25 @@ import pygame
 from engine.events.event import Event
 from engine.focus import Focusable
 from engine.gameobjectcontainer import GameObjectContainer
-from engine.layout import Layout, LayoutAnchor
 from engine.text import Text
 
 
 class Checkbox(GameObjectContainer, Focusable):
     def __init__(
-        self, rect: pygame.Rect, font: pygame.font.Font, color: pygame.Color
+        self, rect: pygame.Rect, font: pygame.font.Font
     ) -> None:
         super().__init__()
 
         self.rect = rect.copy()
         self.font = font
-        self.color = color
-        self.layout = Layout(self.rect)
         self.is_checked = False
-        self.on("click", self.toggle_checked)
-        self.check = Text("V", pygame.Vector2(0, 0), self.font, self.color)
-        self.layout.add(self.check, LayoutAnchor.CENTER, pygame.Vector2(0, 0))
+
+        self.check = Text("V", pygame.Vector2(0, 0), self.font, pygame.Color("white"))
+        self.add_child(self.check)
+        self.check.rect.center = pygame.Rect(0, 0, rect.width, rect.height).center
         self.toggle_v()
-        self.layout.update(0)
+
+        self.on("click", self.toggle_checked)
 
     def render(self, surface: pygame.Surface) -> None:
         if not self.is_visible:
@@ -30,7 +29,7 @@ class Checkbox(GameObjectContainer, Focusable):
         if self.is_checked:
             pygame.draw.rect(
                 surface,
-                pygame.Color("#ffffff"),
+                pygame.Color("#ff8114"),
                 self.absolute_rect,
                 border_radius=8,
             )
@@ -41,15 +40,17 @@ class Checkbox(GameObjectContainer, Focusable):
                 self.absolute_rect,
                 border_radius=8,
             )
+        pygame.draw.rect(
+            surface,
+            pygame.Color("#ffe1c7"),
+            self.absolute_rect,
+            border_radius=8,
+            width=2
+        )
         super().render(surface)
 
     def toggle_v(self) -> None:
-        if self.is_checked:
-            if not self.has_child(self.check):
-                self.add_child(self.check)
-        else:
-            if self.has_child(self.check):
-                self.remove_child(self.check)
+        self.check.is_visible = self.is_checked
 
     def toggle_checked(self, event: Event) -> None:
         if self.is_checked:
