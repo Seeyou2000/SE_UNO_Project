5fc9c1453d66b932e29827fe21a88464ca3ceb46
Kimsiwoo <valentine0517@naver.com>
2023-05-19 01:09:03 +0900

    storyinfomodal 구현

--- game/storyselect/storymodal/storyinfomodal.py
+++ game/storyselect/storymodal/storyinfomodal.py
@@ -1,7 +1,8 @@
 import pygame
 
 from engine.button import Button
-from engine.layout import Horizontal, Vertical
+from engine.events.event import Event
+from engine.layout import Horizontal, Layout, LayoutAnchor, Vertical
 from engine.modal import Modal
 from engine.scene import Scene
 from engine.text import Text
@@ -14,80 +15,78 @@ class StoryInfoModal(Modal):
         self.scene = scene
         self.text_list = text_list
         self.area = area
-        self.display_text_list = []
         self.font = get_font(FontType.UI_BOLD, 20)
+        self.layout = Layout(self.rect)
+        self.display_text_list = []
 
-        for text in self.text_list:
+        self.area_text = Text(
+            self.text_list[0],
+            pygame.Vector2(0, 0),
+            get_font(FontType.UI_BOLD, 40),
+            pygame.Color("black"),
+        )
+        self.layout.add(self.area_text, LayoutAnchor.TOP_LEFT, pygame.Vector2(50, 50))
+        self.add_child(self.area_text)
+
+        for text in self.text_list[1:]:
             self.display_text = Text(
-                text,
+                text.strip(),
                 pygame.Vector2(0, 0),
                 self.font,
                 pygame.Color("black"),
             )
             self.display_text_list.append(self.display_text)
 
-        gap = 20
+        gap = 30
         self.add_child(
             Vertical(
-                pygame.Vector2(30, 30),
+                pygame.Vector2(50, 130),
                 gap,
                 self.display_text_list,
             )
         )
 
-        self.find_story_mode_and_back_button()
-
-    def find_story_mode_and_back_button(self) -> None:
-        from game.ingame.ingamescene import InGameScene
-
-        if self.area == 1:
-            self.enter_button = Button(
-                "입장하기",
-                pygame.Rect(0, 0, 80, 60),
-                self.font,
-                lambda _: self.scene.world.director.change_scene(
-                    InGameScene(self.scene.world, 2, True)
-                ),
-            )
-        elif self.area == 2:
-            self.enter_button = Button(
-                "입장하기",
-                pygame.Rect(0, 0, 80, 60),
-                self.font,
-                lambda _: self.scene.world.director.change_scene(
-                    InGameScene(self.scene.world, 4, False, True)
-                ),
-            )
-        elif self.area == 3:
-            self.enter_button = Button(
-                "입장하기",
-                pygame.Rect(0, 0, 80, 60),
-                self.font,
-                lambda _: self.scene.world.director.change_scene(
-                    InGameScene(self.scene.world, 3, False, False, True)
-                ),
-            )
-        elif self.area == 4:
-            self.enter_button = Button(
-                "입장하기",
-                pygame.Rect(0, 0, 80, 60),
-                self.font,
-                lambda _: self.scene.world.director.change_scene(
-                    InGameScene(self.scene.world, 5, False, False, False, True)
-                ),
-            )
+        self.enter_button = Button(
+            "입장하기",
+            pygame.Rect(0, 0, 150, 60),
+            self.font,
+            self.handle_story_select,
+        )
         self.focus_controller.add(self.enter_button)
 
         self.back_button = Button(
-            "돌아가기", pygame.Rect(0, 0, 80, 60), self.font, lambda _: self.close()
+            "돌아가기", pygame.Rect(0, 0, 150, 60), self.font, lambda _: self.close()
         )
         self.focus_controller.add(self.back_button)
 
-        gap = 300
-        self.add_child(
-            Horizontal(
-                pygame.Vector2(190, 400),
-                gap,
-                [self.enter_button, self.back_button],
-            )
+        buttons = Horizontal(
+            pygame.Vector2(0, 400),
+            50,
+            [self.back_button, self.enter_button],
         )
+
+        self.layout.add(buttons, LayoutAnchor.BOTTOM_CENTER, pygame.Vector2(0, -50))
+        self.add_child(buttons)
+        self.layout.update(0)
+
+    def handle_story_select(self, event: Event) -> None:
+        from game.ingame.ingamescene import InGameScene
+
+        if self.area == 1:
+            self.scene.world.director.change_scene(
+                InGameScene(self.scene.world, 2, True, area_number=1)
+            )
+        elif self.area == 2:
+            self.scene.world.director.change_scene(
+                InGameScene(self.scene.world, 4, False, True, area_number=2)
+            )
+        elif self.area == 3:
+            self.scene.world.director.change_scene(
+                InGameScene(self.scene.world, 3, False, False, True, area_number=3)
+            )
+        elif self.area == 4:
+            self.scene.world.director.change_scene(
+                InGameScene(
+                    self.scene.world, 5, False, False, False, True, area_number=4
+                )
+            )
